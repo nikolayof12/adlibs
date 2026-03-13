c83bc256d5fc836de150307dd93ff021582c4d54
Nikolay Kulikov <nikolayof23@gmail.com>
2026-03-13 11:51:56 +0300

    lib: keyboards: add KEYBOARDS_REGISTER_ENCODER() macro
    Add macro to register encoder object as struct.

--- main/src/lib/keyboards_lib.h
+++ main/src/lib/keyboards_lib.h
@@ -3,10 +3,25 @@
 
 #include <stdint.h>
 
+#include <EncButton.h>
+
 /*
  * Overview
  */
 
+/*
+ * Register new 'struct encoder' static variable to encoder control
+ *
+ * @name - name for new 'struct encoder' obj
+ * @left - pin number, where left turn is connected
+ * @right - pin number, where right turn is connected
+ * @key - pin number, where key (aka button) is connected
+ */
+#define KEYBOARDS_REGISTER_ENCODER(name, left, right, key)		\
+	static EncButton (_enc_ ## name)((left), (right), (key));	\
+	static struct encoder (name) = {				\
+		.obj = &(_enc_ ## name)					\
+	}
 
 struct keyboard_service {
 	uint8_t *buttons;
@@ -21,7 +36,7 @@ struct btn {
 void keyboard_init(struct keyboard_service *service);
 
 /* return 1 if one of bts clicked, else 0 */
-uint8_t keyboard_refresh(struct keyboard_service *service);
-#endif
+uint8_t keyboards_lib_refresh(struct keyboard_service *service);
+
 
 #endif
