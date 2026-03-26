1d675bcdee6a4f9ab8320407e9a5a3f7d325880f
Nikolay Kulikov <nikolayof23@gmail.com>
2026-03-26 18:04:18 +0300

    lib: startup: replace hardcoded sketch upload port with variable
    Add a check for the existence of the device file for the connected
    Arduino.

--- startup.sh
+++ startup.sh
@@ -3,6 +3,23 @@
 #	This script is designed to simplify the dumping of an Arduino project
 #	from the Linux command line.
 
+#TODO: make this script more interactive, such as the ability to specify the
+#      sketch upload port to as a command line argument
+
+
+#If your sketch upload port is named differently, change this variable
+load_device=/dev/ttyUSB0
+
+
+
+
 arduino-cli compile --fqbn arduino:avr:nano ./main/main.ino \
 	--build-property "compiler.cpp.extra_flags=-I./main/include/ -I./main/src/ -I./main/"
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano ./main/main.ino
+
+if [ -e load_device ]
+then
+	arduino-cli upload -p $load_device --fqbn arduino:avr:nano ./main/main.ino
+else
+	echo
+	echo "I can't find a port '$load_device' to upload the sketch to."
+fi
